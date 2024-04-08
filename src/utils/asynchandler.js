76d34d8a7e269a3fc1ba23846195a713ea71abba
Animesh Sahoo <sahoonaimesh29@gmail.com>
2024-04-08 11:58:11 +0530

    utils files created

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,34 @@
+const asynchandler=(requesthandler)=>{
+
+    return ()=>{
+        Promise.resolve(requesthandler(req,res,next)).catch((err)=>next(err));
+    }
+}
+
+
+
+
+
+
+
+export {asynchandler}
+
+
+
+
+
+
+
+
+
+
+// const asynchandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(re, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }

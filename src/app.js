066a3ccb445eefe5069d30253d6b42de06e83e80
Animesh Sahoo <sahoonaimesh29@gmail.com>
2024-05-01 01:09:54 +0530

    controllers and routers are done for user

--- src/app.js
+++ src/app.js
@@ -14,6 +14,9 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+//routes import 
+import userRouter from './routes/user.routes';
+app.use("/api/v1/users",userRouter);
 
 
 
